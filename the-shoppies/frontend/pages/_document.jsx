fed7b9721120804d321aafc7501259645b8646cf
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-12-15 22:07:15 -0500

    style: fix eslint issues

--- the-shoppies/frontend/pages/_document.jsx
+++ the-shoppies/frontend/pages/_document.jsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
+import React from "react";
 
 export default class Document extends NextDocument {
   render() {
--- the-shoppies/frontend/pages/index.jsx
+++ the-shoppies/frontend/pages/index.jsx
@@ -1,4 +1,5 @@
 import { Text } from "@chakra-ui/react";
+import React from "react";
 
 const Index = () => <Text>Hello from Gordon</Text>;
 
