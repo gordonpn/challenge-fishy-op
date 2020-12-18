40377f2876162784bdb05d9b49b88c11c7bdde1a
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-12-18 17:08:29 -0500

    fix: make call to api over https

--- shoppies/frontend/src/api/findMovieByName.js
+++ shoppies/frontend/src/api/findMovieByName.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const findMovieByName = async (searchTerm) => {
   const apiKey = process.env.NEXT_PUBLIC_OMDB_KEY;
   const { data } = await axios.get(
-    `http://omdbapi.com/?apikey=${apiKey}&s=${searchTerm}&type=movie`
+    `https://omdbapi.com/?apikey=${apiKey}&s=${searchTerm}&type=movie`
   );
   return data;
 };
